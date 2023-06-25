90056ad74d958fa49ee3f2eec01d52271acd99c0
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-06-25 21:47:03 +0100

    Added Main Method

---
+++ JavaCalculator.java
@@ -0,0 +1,26 @@
+//Import the Scanner module for readinbg keyboard input
+import java.util.Scanner;
+
+// Container for main
+public class JavaCalculator {
+  //main method declared
+  public static void main(String[] args) {
+    //Creating a new Scanner that can read a users input
+    Scanner scanner = new Scanner(system.in);
+
+    //Enter two integers
+    system.out.print("Enter the First Number: ");
+    int number1 = scanner.nextInt();
+    system.out.print("Enter the Second Number: ");
+    int number2 = scanner.nextInt();
+
+    //Go to sleep, then add operators
+
+    //Add operator functions
+
+    //Add basic error catching
+    
+    //Close the scanner
+    scanner.close();
+  }  
+}
